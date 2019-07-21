3e1ad4cccb9443ce0011bbacef5203d2fd750eb2
ESSch <ESSchtolts@yandex.ru>
2019-07-21 23:48:06 +0300

    work

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-var http = require('http');
+const http = require('http');
 
 function serve(ip, port)
 {
     http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write(JSON.stringify(req.headers));
-        res.end("\nThere's no place like "+ip+":"+port+"\n");
+        res.end(`There's http://${ip}:${port}\n`);
     }).listen(port, ip);
-    console.log('Run http://'+ip+':'+port+'/');
+    console.log(`Run http://${ip}:${port}`);
 }
 
 serve('0.0.0.0', 9000);
